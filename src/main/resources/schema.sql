23e113bf43823f1e5f483188323a0fc3096a5e55
Dokuru Bhanu Prasad Reddy <bhanuprasad868reddy@gmail.com>
2025-11-20 11:36:58 +0530

    changed schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS employee (
-  id BIGSERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(150) UNIQUE NOT NULL,
-  position VARCHAR(100),
-  sick_leaves INTEGER DEFAULT 0,
-  casual_leaves INTEGER DEFAULT 0,
-  earned_leaves INTEGER DEFAULT 0
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(150) UNIQUE,
+    position VARCHAR(100),
+    sick_leaves INT DEFAULT 0,
+    casual_leaves INT DEFAULT 0,
+    earned_leaves INT DEFAULT 0
 );
 
